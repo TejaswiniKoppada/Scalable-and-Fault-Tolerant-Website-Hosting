957529c4852e56d9bcf8a9e71f8240862c20cd74
Tejaswini Koppada <140075679+TejaswiniKoppada@users.noreply.github.com>
2025-05-10 19:29:25 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,27 @@
+n=input().split()
+l=[]
+for i in n:
+    j=i.split("/")
+    l.append([int(j[0]),int(j[1])])
+print(l)
+    
+#num1=input("enter the first num: ")
+#num2=input("enter the second num: ")
+#result=add_num(num1,num2)
+#print("Result:",result)
+num=[1,2,3,4,5]
+for i in num:
+    if i%2==0:
+        print("even")
+    else:
+        print("odd")
+
+num=[1,2,3,4,5]
+for i in num:
+    if i%2==0:
+        print("even")
+    else:
+        print("odd")
+
+def ana(str1,str2):
+    return
